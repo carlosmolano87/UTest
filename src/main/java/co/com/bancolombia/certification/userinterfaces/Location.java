17b554e7cce728a9405b74dabea1e5799d3958a5
carlosmolano87 <carlosmolano4455@gmail.com>
2022-11-19 16:22:33 -0500

    evidences

--- src/main/java/co/com/bancolombia/certification/userinterfaces/Location.java
+++ src/main/java/co/com/bancolombia/certification/userinterfaces/Location.java
@@ -3,6 +3,11 @@ package co.com.bancolombia.certification.userinterfaces;
 import net.serenitybdd.screenplay.targets.Target;
 
 public class Location {
-    public static final Target BUTTONLOCATION = Target.the("button next").
-            locatedBy("//a[@class=\"btn btn-blue\"]");
+
+    public static final Target COUNTRY = Target.the("sistema").
+            locatedBy("(//span[@class=\"btn btn-default form-control ui-select-toggle\"])[2]");
+    public static final Target COLOMBIA = Target.the("windows").
+            locatedBy("(//span[@class=\"ui-select-choices-row-inner\"])[48]");
+    public static final Target NEXT = Target.the("button next").
+            locatedBy("//a[@class=\"btn btn-blue pull-right\"]");
 }
--- src/main/java/co/com/bancolombia/certification/utest/tasks/RegisterUser.java
+++ src/main/java/co/com/bancolombia/certification/utest/tasks/RegisterUser.java
@@ -5,10 +5,20 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.SendKeys;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Sleeper;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
 public class RegisterUser implements Task {
+
+    static WebDriver driver;
+    static WebDriverWait wait;
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
@@ -19,19 +29,22 @@ public class RegisterUser implements Task {
                 SendKeys.of("July").into(Personal.MONTH),
                 SendKeys.of("5").into(Personal.DAY),
                 SendKeys.of("2000").into(Personal.YEAR),
-                Click.on(Location.BUTTONLOCATION),
+                Click.on(Personal.NEXTLOCATION),
+                Click.on(Location.COUNTRY),
+                Click.on(Location.COLOMBIA),
+                Click.on(Location.NEXT),
                 Click.on(Devices.SISTEMA),
                 Click.on(Devices.WINDOWS),
                 Click.on(Devices.VERSION),
                 Click.on(Devices.XP),
                 Click.on(Devices.LANGUAGE),
                 Click.on(Devices.ENGLISH),
-                Click.on(Devices.BUTTONFINAL),
-                SendKeys.of("Andres2022****").into(Complete.PASSWORD),
-                SendKeys.of("Andres2022****").into(Complete.CONFIRMPASSWORD),
-                Click.on(Complete.CHECKONE),
-                Click.on(Complete.CHECKTWO),
-                Click.on(Complete.BUTTONCOMPLETE)
+                Click.on(Devices.BUTTONFINAL)
+                //SendKeys.of("Andres2022****").into(Complete.PASSWORD),
+                //SendKeys.of("Andres2022****").into(Complete.CONFIRMPASSWORD),
+                //Click.on(Complete.CHECKONE),
+                //Click.on(Complete.CHECKTWO),
+                //Click.on(Complete.BUTTONCOMPLETE)
         );
     }
 
